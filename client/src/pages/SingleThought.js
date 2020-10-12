b0fd4500d6c4227dc68a39b2de9ef2f629c28c2c
DWill1440 <dwill2think@gmail.com>
2020-10-12 13:13:58 -0500

    ReactionForm

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom';
 
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionForm from '../components/ReactionForm';
 
+import Auth from '../utils/auth';
 import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
@@ -34,6 +36,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reationCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
